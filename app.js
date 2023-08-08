78aac0559b89a28b2ee2adf3e5b81cecdfdffe90
ForVic <victor.lakers0@gmail.com>
2023-08-07 23:56:24 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import AuthController from "./users/auth-controller.js";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from "cors";
+import session from "express-session";
+import "dotenv/config";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+app.use(express.json());
+TuitsController(app);
+HelloController(app);
+UserController(app);
+AuthController(app);
+app.listen(process.env.PORT || 4000);
